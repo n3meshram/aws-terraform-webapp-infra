1ab53a324ca68eaa6cbec7d7229350318d5deea0
Nitin Meshram <ntnmeshram1@gmail.com>
2026-03-24 14:05:02 +0000

    stabilized infra, fixed SG lifecycle, added pipeline-ready structure

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group" "alb_sg" {
-  name        = "webapp-${var.environment}-alb-sg"
+  name_prefix        = "webapp-${var.environment}-alb-sg"
   description = "Security group for ALB allowing HTTP traffic from internet"
   vpc_id      = var.vpc_id
-  
+
   #tfsec:ignore:aws-ec2-no-public-ingress-sgr
   ingress {
     description = "Allow HTTP traffic from internet to ALB"
@@ -11,26 +11,29 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "webapp-${var.environment}-alb-sg"
+  }
 }
 
-resource "aws_security_group_rule" "alb_to_ec2_egress" {
-  description              = "Allow HTTP traffic from ALB to EC2"
-  type                     = "egress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
 
-  security_group_id        = aws_security_group.alb_sg.id
-  source_security_group_id = aws_security_group.ec2_sg.id
-}
 
 resource "aws_security_group" "ec2_sg" {
-  name        = "webapp-${var.environment}-ec2-sg"
+  name_prefix        = "webapp-${var.environment}-ec2-sg"
   description = "Security group for EC2 allowing traffic from ALB"
   vpc_id      = var.vpc_id
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
   tags = {
-    Name = "webapp-${var.environment}-ec2-sg"
+    Name = "webapp-${var.environment}-alb-sg"
   }
 }
 
@@ -43,4 +46,15 @@ resource "aws_security_group_rule" "alb_to_ec2" {
 
   security_group_id        = aws_security_group.ec2_sg.id
   source_security_group_id = aws_security_group.alb_sg.id
+}
+
+resource "aws_security_group_rule" "alb_to_ec2_egress" {
+  description              = "Allow HTTP traffic from ALB to EC2"
+  type                     = "egress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+
+  security_group_id        = aws_security_group.alb_sg.id
+  source_security_group_id = aws_security_group.ec2_sg.id
 }
