f6709bc937f4aef4c3478354dd645ac4f187c095
Nitin Meshram <ntnmeshram1@gmail.com>
2026-03-24 10:53:05 +0000

    remove 1 security issue

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -7,14 +7,13 @@ resource "aws_security_group" "alb_sg" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    
+  }
 
   egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
     security_groups = [aws_security_group.ec2_sg.id]
-  }
   }
 
   tags = {
