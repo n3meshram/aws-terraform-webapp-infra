2ccdbd4b2ea6c70c37b2fed7f7dd8b757236cabd
Nitin Meshram <ntnmeshram1@gmail.com>
2026-03-29 05:31:30 +0000

    Move to runtime SSM fetch2

--- modules/launch-template/variables.tf
+++ modules/launch-template/variables.tf
@@ -3,7 +3,3 @@ variable "ami_id" {}
 variable "instance_type" {}
 variable "security_group_id" {}
 variable "instance_profile_name" {}
-variable "app_password" {
-  description = "App password from SSM"
-  type        = string
-}
