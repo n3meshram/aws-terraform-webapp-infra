85a361603408cbe73a4a0e5c29bfadc814b358d0
Nitin Meshram <ntnmeshram1@gmail.com>
2026-03-30 10:35:03 +0000

    dev testing

--- environments/stage/variables.tf
+++ environments/stage/variables.tf
@@ -18,3 +18,6 @@ variable "project" {
   type        = string
 }
 
+
+
+
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "this" {
   enable_dns_support   = true
 
   tags = {
-    Name        = "webapp-${var.environment}-vpc"
+    Name        = "webapp-${var.environment}-vpc@"
     Environment = var.environment
   }
 }
