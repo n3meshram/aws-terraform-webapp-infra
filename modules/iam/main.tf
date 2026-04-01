d10855a4ec10ec4f43efb38b18d05f0b311e5f40
Nitin Meshram <ntnmeshram1@gmail.com>
2026-04-01 10:05:13 +0000

    add secrets

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -45,3 +45,21 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2-ssm-profile-${var.environment}"
   role = aws_iam_role.ec2_ssm_role.name
 }
+
+resource "aws_iam_role_policy" "secrets_access" {
+  name = "secrets-access-${var.environment}"
+  role = aws_iam_role.ec2_ssm_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
